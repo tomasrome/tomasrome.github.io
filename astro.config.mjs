5bcfe73e1b1e3cbb942d1e790c30b9640e82039c
TomasRom <tomasrom.dev@gmail.com>
2024-07-02 23:45:32 -0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://tomasrome.github.io",
+  integrations: [sitemap()],
 });
